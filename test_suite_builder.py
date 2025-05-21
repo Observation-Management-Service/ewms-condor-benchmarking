838191fb360ad561f6e0005992474e5f8c8a41aa
ric-evans <emejqz@gmail.com>
2025-05-21 17:59:13 -0500

    fixed-width, visually aligned filename from vars - 5

--- test_suite_builder.py
+++ test_suite_builder.py
@@ -81,7 +81,7 @@ def get_fname(prefix: str, vars: dict[str, Any], suffix: str) -> str:
         str_val = str_val.lower()
 
         # Pad the abbrev and value to fixed widths
-        middle_parts.append(f"{first_letters}_{str_val:<5}".replace(" ", "_"))
+        middle_parts.append(f"{first_letters}_{str_val:_<5}")
 
     middle_string = "_".join(middle_parts).rstrip("_")
     return f"{prefix}_{middle_string}{suffix}"
