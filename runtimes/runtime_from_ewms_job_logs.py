c8f52ca22c94b454085ead0585e51cb9dc6c8aa1
ric-evans <emejqz@gmail.com>
2025-06-24 17:34:21 -0500

    get starttimes

--- runtimes/runtime_from_ewms_job_logs.py
+++ runtimes/runtime_from_ewms_job_logs.py
@@ -1,8 +1,11 @@
 """Get the end times (time of last finished task) for each taskforce."""
 
 import argparse
+import asyncio
 from datetime import datetime
 from pathlib import Path
+
+from rest_tools.client import RestClient, SavedDeviceGrantAuth
 
 
 def get_final_time_for_taskforce(tf_path: Path) -> datetime | None:
@@ -30,7 +33,21 @@ def get_final_time_for_taskforce(tf_path: Path) -> datetime | None:
     return latest_time
 
 
-def main():
+async def get_creation_time_for_wf(rc: RestClient, tf_dname: str) -> datetime:
+
+    def _to_workflow_id(tf_dir: str) -> str:
+        # ewms-taskforce-TF-685643b1-7a50f872-ba2017b8-05bfe8b1 -> WF-685643b1-7a50f872
+        parts = tf_dir.split("-")
+        return f"WF-{parts[2]}-{parts[3]}"
+
+    workflow_id = _to_workflow_id(tf_dname)
+
+    resp = await rc.request("GET", f"/v1/workflows/{workflow_id}")
+
+    return datetime.fromtimestamp(resp["timestamp"])
+
+
+async def main():
     parser = argparse.ArgumentParser(
         description="Get end times for one or more taskforces."
     )
@@ -41,6 +58,14 @@ def main():
     )
     args = parser.parse_args()
 
+    rc = SavedDeviceGrantAuth(
+        "https://ewms-dev.icecube.aq",
+        token_url="https://keycloak.icecube.wisc.edu/auth/realms/IceCube",
+        filename=str(Path("~/ewms-dev-device-refresh-token").expanduser().resolve()),
+        client_id="ewms-dev-public",
+        retries=0,
+    )
+
     # Build list of taskforce directories to process
     if args.dir.name.startswith("ewms-taskforce-TF-"):
         tf_dirs = [args.dir]
@@ -49,17 +74,20 @@ def main():
     tf_dirs = [p.resolve() for p in tf_dirs]
     print(f"looking at {[str(d) for d in tf_dirs]}...")
 
-    results: dict[str, datetime] = {}
-
-    # parse
+    # get endtimes
+    endtimes: dict[str, datetime] = {}
     for tf_path in tf_dirs:
         final_time = get_final_time_for_taskforce(tf_path)
-        results[tf_path.name] = final_time
+        endtimes[tf_path.name] = final_time
+    for tf_id in sorted(endtimes):
+        print(f"{tf_id} {endtimes[tf_id].isoformat(sep=' ')}")
 
-    # print
-    for tf_id in sorted(results):
-        print(f"{tf_id} {results[tf_id].isoformat(sep=' ')}")
+    # get start times
+    starttimes: dict[str, datetime] = {}
+    for tf_path in tf_dirs:
+        starttimes[tf_path.name] = await get_creation_time_for_wf(rc, tf_path.name)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
+    print("Done.")
